ff7742ff34025cf0e0567968dee00db288c2bfa0
rokari-19 <oshomahiyelobu1@gmail.com>
2024-10-24 22:44:50 +0100

    new prod branch, setting up for postgresql on amazon rds, vercel hosting

--- em_system/wsgi.py
+++ em_system/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'em_system.settings')
 
 application = get_wsgi_application()
+
+app = application
